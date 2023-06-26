872acd137d6fdbb2cdeab7d7cdd9f5fc5787b2e2
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-06-26 10:04:09 +0000

    [First]Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
