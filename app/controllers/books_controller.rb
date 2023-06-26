7a5bd902c692fd0765975d08cb15e36998d333d9
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-06-26 16:07:36 +0000

    [Second]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to '/books/#{book.id}'
   end
 
   def index
@@ -15,11 +15,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
