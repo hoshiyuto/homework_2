d3149c672ac51a1f299638d883efaeec3350dda1
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-06-27 10:54:00 +0000

    [5th]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully crreated."
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully crreated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
